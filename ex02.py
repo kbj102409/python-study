53b2f6ca7db068a998d7c9ce7286efbaecb94c0f
kbj102409 <91105962+kbj102409@users.noreply.github.com>
2021-10-01 17:22:40 +0900

    Add files via upload

---
+++ ex02.py
@@ -0,0 +1,56 @@
+# 가사를 코딩으로
+print('''아버지 날 보고있다면 정답을 알려줘 
+어른이 되기엔 난 여리고 여려 아직도 
+정답을 모르고
+부딪히는 짓만 하기엔 너무 ''')
+
+# a의 값 출력
+a = 10
+print("a の数は", a)
+
+# 자기소개
+name = "キムビョンジュン"
+age = 25
+email = "kjs102409@naver.com"
+hobby = "ユーチューブ試聴"
+print("名前は", name)
+print("年齢は", age)
+print("メールは", email)
+print("趣味は", hobby)
+
+# "type"과"print"사용법 및 유형
+type(3)
+type(3.12)
+type("korea")
+print(type(3))
+print(type(3.12))
+print(type("korea"))
+
+# 연산자 활용
+a =10
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+A = "hello"
+B = "world"
+print("Aは",A,"です.")
+print("Bは",B,"です.")
+print()
+
+# spurs go champions~~~~~
+A = "spurs go"
+B = "champions"
+C = A+" "+B*3+"~"*5
+print(C)
+
+# 각 자리수 출력
+a = 568
+print(a//1000)
+print((a//100)%10)
+print((a//10)%10)
+print(a%10)
